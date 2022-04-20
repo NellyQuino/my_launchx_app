4283e9062c645fa432f7958ccfc7ea803fafc74d
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-20 13:14:03 -0700

    create folder app and test

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+
+
+const carlo = new MissionCommander("Carlo")
+const nelly = new MissionCommander("Nelly")
+const luis = new MissionCommander("Luis")
+console.log(carlo.name)
+console.log(nelly.name)
+console.log(luis.name)
